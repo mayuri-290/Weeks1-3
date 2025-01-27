d937decd6346926d67ccab4bd6d1d6b461f53ef9
Ziting Sun <sunziti@shernet.sheridancollege.ca>
2025-01-26 23:12:29 -0500

    added comments to the planet rotation

--- Assets/MechanicalDrawing/scripts/planet1Rotation.cs
+++ Assets/MechanicalDrawing/scripts/planet1Rotation.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class planet1Rotation : MonoBehaviour
 {
-    public float speed = 10.0f;
+    public float speed = 10.0f; //the speed at which the object rotates.
 
     // Start is called before the first frame update
     void Start()
@@ -15,8 +15,8 @@ public class planet1Rotation : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 rot = transform.eulerAngles;
-        rot.z += speed * Time.deltaTime;
-        transform.eulerAngles = rot;
+        Vector3 rot = transform.eulerAngles;//set and declair the variables: rot
+        rot.z += speed * Time.deltaTime;//add z axis to make it rotate
+        transform.eulerAngles = rot;//apply the rotation to object
     }
 }
